9ac6b71858fb3e19ef3d36cfb75cac333e5793b3
Nick Nicolosi <nnicolosi@singlestoneconsulting.com>
2021-04-18 21:11:01 -0400

    Add Postgres DB integration with TypeORM

---
+++ server/src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { DatabaseModule } from '../database/database.module';
+import { Module } from '@nestjs/common';
+import { userProviders } from './user.providers';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [...userProviders, UserService],
+})
+export class UserModule {}
