c1ad4e3c3e9ac2a61680211053d5e7e420ee433d
Nick Nicolosi <nick.nicolosi@protonmail.com>
2021-06-23 22:41:37 -0400

    Lighten up navbar

--- client/src/components/navigation/index.jsx
+++ client/src/components/navigation/index.jsx
@@ -3,7 +3,7 @@ import './navigation.scss';
 
 const Navigation = () => {
   return (
-    <nav className="navbar is-black" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
           <img src="/favicon-32x32.png" alt="Logo" width="28" height="28" />
@@ -12,7 +12,7 @@ const Navigation = () => {
       </div>
 
       <div className="navbar-end">
-        <NavLink className="navbar-item" to="/login">
+        <NavLink className="navbar-item" to="/login" activeClassName="active">
           Login
         </NavLink>
       </div>
